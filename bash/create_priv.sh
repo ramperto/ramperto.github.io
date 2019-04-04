7cc8ccf3f054d3d5e6ba22c2a502c0ef9a2d4473
ramperto devops <rampertodevops@Memberids-MacBook-Pro-3.local>
2019-04-04 16:34:35 +0700

    try 9

--- bash/create_priv.sh
+++ bash/create_priv.sh
@@ -5,3 +5,5 @@ cp private ~/.ssh/id_rsa
 chmod 700 ~/.ssh && chmod 600 ~/.ssh/id_rsa
 cp known_hosts ~/.ssh/ && chmod 644 ~/.ssh/known_hosts
 ls -al ~
+ssh rizki@188.166.179.150 -p5858 'whoami; hostname'
+
